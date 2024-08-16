4a8bf169ae529245c4f098eca094c0200ca33266
Aakash Tembhare <82498272+21-aakash@users.noreply.github.com>
2024-08-16 16:56:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ load_dotenv()
 api_key = os.getenv("GROQ_API_KEY")
 
 st.set_page_config(page_title="LangChain: Chat with SQL DB", page_icon="🦜")
-st.title("👽 SkySQL-LangChain: Chat with SQL Database")
+st.title("👽 SkySQL-LangChain")
 
 LOCALDB = "USE_LOCALDB"
 MYSQL = "USE_MYSQL"
